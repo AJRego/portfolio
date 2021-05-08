18b45a086007dab8afba856d206aa0e1f86863e7
AJRego <rego.andre99@gmail.com>
2021-05-08 14:31:14 -0500

    Change tab content when you click a tab item

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* ------------------- about section tabs ------------------------- */
+
+(() => {
+  const aboutSection = document.querySelector(".about-section");
+  const tabsContainer = document.querySelector(".about-tabs");
+
+  tabsContainer.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("tab-item") &&
+      !event.target.classList.contains("active")
+    ) {
+      const target = event.target.getAttribute("data-target");
+      // desactive existing active 'tab-item'
+      tabsContainer
+        .querySelector(".active")
+        .classList.remove("outer-shadow", "active");
+      // active new 'tab-item'
+      event.target.classList.add("active", "outer-shadow");
+      // desactive existing active 'tab-content'
+      aboutSection.querySelector(".tab-content.active").classList.remove("active");
+      // active new 'tab-content'
+      aboutSection.querySelector(target).classList.add("active")
+    }
+  });
+})();
