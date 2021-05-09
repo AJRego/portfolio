684fa4e400c27575de53c6e6de1e6099a44f6db3
AJRego <rego.andre99@gmail.com>
2021-05-09 07:26:30 -0500

    Add event to prev and next buttons

--- js/main.js
+++ js/main.js
@@ -190,3 +190,51 @@ function bodyScrollingToggle() {
     }
   }
 })();
+
+/* ------------------- testimonial slider ------------------------- */
+
+(() => {
+  const sliderContainer = document.querySelector(".testi-slider-container");
+  const slides = sliderContainer.querySelectorAll(".testi-item");
+  const slideWidth = sliderContainer.offsetWidth;
+  const prevBtn = document.querySelector(".testi-slider-nav .prev");
+  const nextBtn = document.querySelector(".testi-slider-nav .next");
+  const activeSlide = sliderContainer.querySelector(".testi-item.active");
+
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+  // set width of all slides
+  slides.forEach((slide) => {
+    slide.style.width = slideWidth + "px";
+  });
+  // set widtg of slider container
+  sliderContainer.style.width = slideWidth * slides.length + "px";
+
+  nextBtn.addEventListener("click", () => {
+    if (slideIndex === slides.length - 1) {
+      slideIndex = 0;
+    } else {
+      slideIndex++;
+    }
+    slider();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (slideIndex === 0) {
+      slideIndex = slides.length - 1;
+    } else {
+      slideIndex--;
+    }
+    slider();
+  });
+
+  function slider() {
+    // deactivate existing active slides
+    sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+    // activate new slide
+    slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = -(slideWidth * slideIndex) + "px";
+  }
+  slider();
+
+})();
