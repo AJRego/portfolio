9e235b7147f6ec3a19dc988da7619e4027aa296f
AJRego <rego.andre99@gmail.com>
2021-05-09 15:28:43 -0500

    Fix theme when reload the page

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -3,15 +3,33 @@
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
   document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+  updateIcon();
 });
 
-window.addEventListener("load", () => {
+function themeMode() {
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+  updateIcon();
+}
+themeMode();
+
+function updateIcon() {
   if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.remove("fa-moon");
     dayNight.querySelector("i").classList.add("fa-sun");
   } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
     dayNight.querySelector("i").classList.add("fa-moon");
   }
-});
+}
